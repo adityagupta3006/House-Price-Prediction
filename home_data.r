c03b9dedd0d778b93df23610284cf84db2769a22
Aditya Gupta <agupta42@uncc.edu>
2017-06-19 23:09:47 -0400

    modified indentation

--- home_data.r
+++ home_data.r
@@ -1,25 +1,25 @@
 library(lattice) 
 library(corrplot)
 graphics.off      
-cat("\014")                      						# Clears screen
-rm(list=ls())																# Clears environment
+cat("\014")      # Clears screen
+rm(list=ls())		 # Clears environment
 setwd(getwd())
 mydataset <- as.data.frame(read.csv(file="home_data.csv", header=T, sep=","))	# Reading CSV File
 
 #-------------------------------------Data Cleaning Function--------------------------------------------------------------------------
-clean <- function(x){														# Create a function for dataset cleaning
+clean <- function(x){	# Create a function for dataset cleaning
   suppressWarnings(as.numeric( gsub('[^a-zA-Z0-9.]', '', x)))	# Remove all unneccessary symbols and convert into numeric	
 }
-mydataset[] <- sapply(mydataset, clean)										# Call function using sapply on dataset
-mydatasetcla <- subset( mydataset, select = -c(id, date ) )					# Removing House id and date
+mydataset[] <- sapply(mydataset, clean) # Call function using sapply on dataset
+mydatasetcla <- subset( mydataset, select = -c(id, date ) )	# Removing House id and date
 #-------------------------------------------------------------------------------------------------------------------------------------
 
-#splom(mydatasetcla)														# Creating ScatterPlots
+#splom(mydatasetcla) # Creating ScatterPlots
 for (i in 2:ncol(mydatasetcla)){
   plot(mydatasetcla[,i], mydatasetcla$price, ylab="price", xlab = colnames(mydatasetcla)[i])
 }
 
-cor(mydatasetcla)[1,]														# Correlation matrix for Price
+cor(mydatasetcla)[1,]	# Correlation matrix for Price
 
 #Plotting correlation matrix
 corrplot(cor(mydatasetcla), method="number")
@@ -37,11 +37,11 @@ summary(linearModel)
 #Updating Linear Model to eliminate unncessary variables
 linearModel <- update(linearModel, .~.-sqft_basement-floors-sqft_lot-sqft_lot15
                       -sqft_living15-yr_renovated)
-#residuals(linearModel)												# Prints residuals of linearModel
+#residuals(linearModel)	# Prints residuals of linearModel
 summary(linearModel)
 
 
-pred <- predict(linearModel,test_data )				# Prediction of prices using linear model
+pred <- predict(linearModel,test_data )	# Prediction of prices using linear model
 
 actuals_preds <- data.frame(cbind(actuals=test_data$price, predicteds=pred))
 correlation_accuracy <- cor(actuals_preds)  
